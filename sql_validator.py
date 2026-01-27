3538bc54c6bb8a025b4f46d63bfc39cb00646bc6
Arman Tiwari <tiwariar279@gmail.com>
2026-01-27 22:54:55 +0530

    Add dangerous SQL keywords to SQLValidator class

--- sql_validator.py
+++ sql_validator.py
@@ -1,1 +1,11 @@
+import re
+from typing import Tuple
 
+
+class SQLValidator:
+    DANGEROUS_KEYWORDS = [
+        'INSERT', 'UPDATE', 'DELETE', 'DROP', 'ALTER', 'CREATE',
+        'TRUNCATE', 'REPLACE', 'RENAME', 'GRANT', 'REVOKE',
+        'COMMIT', 'ROLLBACK', 'SAVEPOINT', 'EXEC', 'EXECUTE',
+        'ATTACH', 'DETACH', 'PRAGMA'
+    ]
