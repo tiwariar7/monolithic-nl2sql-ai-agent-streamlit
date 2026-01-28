b2cf043833fe898966a91c8a2add0f9bf995d594
Arman Tiwari <tiwariar279@gmail.com>
2026-01-28 09:53:46 +0530

    Implement DataLoader class with DuckDB connection
    Add DataLoader class for loading data from DuckDB

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,8 @@
+import duckdb
+import pandas as pd
+import os
 
+class DataLoader:  
+    def __init__(self, db_path: str = ":memory:"):
+        self.conn = duckdb.connect(db_path)
+        self.loaded_tables: List[str] = [] 
